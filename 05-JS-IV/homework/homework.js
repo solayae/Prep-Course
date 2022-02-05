8ba022f5d155646797ffd9cf8ecd4ccb17eb571a
Sebastian Olaya <sebastianolayae@gmail.com>
2022-02-04 21:22:56 -0500

    02 JS-1 finalizado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,9 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  let toni = {
+    nombre
+  }
 }
 
 
